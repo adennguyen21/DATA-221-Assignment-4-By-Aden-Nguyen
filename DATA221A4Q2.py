f9dae8bb4f1d60494f05b583413cf26e5790b9bc
adennguyen21 <adennguyen21@gmail.com>
2026-03-22 16:27:21 -0600

    Train model and print test and train accuracies.

--- DATA221A4Q2.py
+++ DATA221A4Q2.py
@@ -13,3 +13,17 @@ target_vector_y = breast_cancer_data.target
 
 # Split dataset
 feature_train, feature_test, target_train, target_test = train_test_split(feature_matrix_x, target_vector_y, test_size=0.2, stratify = target_vector_y, random_state=42)
+
+# Initialize and train decision tree model
+decision_tree_classifier = DecisionTreeClassifier(criterion = "entropy", random_state = 42)
+decision_tree_classifier.fit(feature_train, target_train)
+
+# Print test and train accuracies
+train_accuracy = decision_tree_classifier.score(feature_train, target_train)
+test_accuracy = decision_tree_classifier.score(feature_test, target_test)
+print(f"Training accuracy: {train_accuracy}")
+print(f"Test accuracy: {test_accuracy}")
+
+
+
+
