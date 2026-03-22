026dd1e209c92856021ae7d965659a6d46eb8381
adennguyen21 <adennguyen21@gmail.com>
2026-03-22 15:48:10 -0600

    Print number of samples belonging to each class.

--- DATA221A4Q1.py
+++ DATA221A4Q1.py
@@ -1,5 +1,5 @@
 # Question 1: Dataset Exploration and Understanding
-
+import numpy as np
 from sklearn.datasets import load_breast_cancer
 
 # Load dataset
@@ -10,3 +10,10 @@ target_vector_y = breast_cancer_data.target
 # Print Shapes of X and y.
 print(f"Shape of X: {feature_matrix_x.shape}")
 print(f"Shape of y: {target_vector_y.shape}")
+
+# Print number of samples belonging to each class
+unique, counts = np.unique(target_vector_y, return_counts=True)
+print(f"Class distribution: {dict(zip(unique, counts))}")
+
+# ======================================================================
+# Questions:
